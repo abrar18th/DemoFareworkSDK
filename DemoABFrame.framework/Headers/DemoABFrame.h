dc686ee52122f6e0b6f845d8f2bb2c1d0a3141f9
IMiMineDigital <48542324+IMiMineDigital@users.noreply.github.com>
2020-05-06 18:20:34 +0530

    Commit DemoFramework

---
+++ DemoABFrame.framework/Headers/DemoABFrame.h
@@ -0,0 +1,19 @@
+//
+//  DemoABFrame.h
+//  DemoABFrame
+//
+//  Created by Abrar Ali on 30/04/20.
+//  Copyright © 2020 Abrar Ali. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoABFrame.
+FOUNDATION_EXPORT double DemoABFrameVersionNumber;
+
+//! Project version string for DemoABFrame.
+FOUNDATION_EXPORT const unsigned char DemoABFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoABFrame/PublicHeader.h>
+
+
